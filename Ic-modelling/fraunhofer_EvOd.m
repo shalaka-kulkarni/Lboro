11899d5c0c735f43b8ddbd3b2781d830daa9a7ca
Shalaka <shalaka-kulkarni@users.noreply.github.com>
2016-05-26 17:39:50 +0100

    Separate into even and odd parts of Ic_max - flip fn doesn't work

---
+++ Ic-modelling/fraunhofer_EvOd.m
@@ -0,0 +1,30 @@
+%constant definitions
+fluxQuantum = 2.06783383E-15;
+lambda = 39E-9; %London penetration depth of Niobium
+
+%geometry
+d = 10E-9; L = 100E-9;
+
+%physical quantities
+width = 0.16; N=500;
+B = linspace(-width,width,N);
+beta = 2*pi*(2*lambda+d)*B/fluxQuantum;
+I0 = 200E-6; %peak
+
+Ic = @(x)I0*abs(sinc(x*L*(2*lambda+d)/fluxQuantum));
+
+Ic_max = Ic(beta);
+plot(beta, Ic_max);
+
+[I_even, minX] = flipFn(Ic_max,beta);
+%plot(beta,I_even);
+
+I_odd = interp1(beta, I_even, minX);
+
+Ix = I_even + 1j*I_odd;
+
+Jx = ifft(Ix);
+
+%plot(beta, Ic_max, 'r', beta, I_even, 'g', beta, I_odd, 'b');
+%plot(beta, Jx);
+
