8466aca55034526d7ba109ce3756aa85de18ed67
Shalaka <shalaka-kulkarni@users.noreply.github.com>
2016-05-26 17:37:03 +0100

    My own IFFT integral - doesn't work
    Syntactical pains

--- Ic-modelling/fraunhofer.m
+++ Ic-modelling/fraunhofer.m
@@ -1,3 +1,9 @@
+%--------------------------------------------------------------------
+%This version of the code contains use of the inbuilt hilbert function, and
+%my own integral equation
+%Error @ line 36 : "Index exceeds matrix dimensions"
+%--------------------------------------------------------------------
+
 %constant definitions
 u0 = 1.25663706E-6;
 ur = 150; %relative permeability of the material
@@ -9,37 +15,29 @@ d = 10E-9; L = 100E-9;
 
 %physical quantities
 width = 1; N=500;
-B = linspace(-width,width,N);
+B = linspace(-width,width,N); %magnetic field vector
 flux = B*L*(2*lambda + d);
 I0 = 200E-6; %peak
 
-dim = linspace(-1E-9,1E-9,N);
+dim = linspace(-d,d,N); %arbitrary length dimension vector
 
 Ic = @(x)I0*abs(sinc(x*L*(2*lambda+d)/fluxQuantum));
 beta = 2*pi*(2*lambda+d)*B/fluxQuantum;
 
-%theta = @(x)imag(hilbert(log(Ic(x))));
-f = @(x,y)(x/(2*pi))*(log(Ic(y)) - log(Ic(x)))/(x^2 - y^2);
-for k=1:N
-    theta
-end
+%implementing eqn(5) from the paper
+theta = @(x)imag(hilbert(log(Ic(x))));
+
 plot(beta,Ic(B))
 plot(beta,theta(beta));
     
-%transform = zeros(size(B));
-Ix = zeros(size(B));
+ft = zeros(size(B));
 for k=1:N
-   %transform = @(x)Ic(x).*exp(1j*theta(x)); 
+   %constructing the exponential part with which Ic(beta) is multiplied in
+   %eqn(6) from the paper
    exp_part = @(x)exp(1i*(theta(beta(k))-beta(k)*x));
+   
+   %implementing eqn(6) from the paper
    ft = @(x)(0.5/pi)*integral(@(beta)Ic(beta(k))*exp_part(x),beta(1),beta(N));
-   Ix(k) = ft(dim(k));
 end
 
-%Ix = ifft(transform(beta));
-
-%plot(beta,Ix,'r',beta,Ic(B),'g');
-%plot(beta,Ix);
-%plot(beta,log(Ic(beta)));
-%plot(dim,transform(beta));
-%plot(beta,fft(Ix));
 plot(dim,ft(dim));
