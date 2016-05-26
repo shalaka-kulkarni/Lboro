5640ab83bfd8be41afeea65b6aaa3983f1a5cac5
Shalaka <shalaka-kulkarni@users.noreply.github.com>
2016-05-26 17:36:12 +0100

    Cleaned up code

--- Ic-modelling/fraunhofer.m
+++ Ic-modelling/fraunhofer.m
@@ -5,40 +5,41 @@ fluxQuantum = 2.06783383E-15;
 lambda = 39E-9; %London penetration depth of Niobium
 
 %geometry
-d = 10E-9;
-L = 100E-9;
+d = 10E-9; L = 100E-9;
 
 %physical quantities
 width = 1; N=500;
 B = linspace(-width,width,N);
 flux = B*L*(2*lambda + d);
-I0 = 200E-6; %peak current
+I0 = 200E-6; %peak
+
+dim = linspace(-1E-9,1E-9,N);
 
 Ic = @(x)I0*abs(sinc(x*L*(2*lambda+d)/fluxQuantum));
-
-%funx = @(b,c) (c./(2*pi))*(log(Ic(b)) - log(Ic(c)))./(c.^2 - b.^2);
-
 beta = 2*pi*(2*lambda+d)*B/fluxQuantum;
 
-%for i=1:N
-    %theta(i) = integral(@(b)funx(b,beta(i)),-Inf,Inf,'PrincipalValue',true); 
-    %this is becoming an infi integral. doesnt the integral fn handle limits?
-    
-%     syms b1 c1;
-%     f = (beta(i)/(2*pi))*(log(Ic(b1)) - log(Ic(beta(i))))/(beta(i)^2 - b1^2);
-%     thetaval = int(f,b1,-2E8,2E8,'PrincipalValue',true); 
-%     thetaval
-
-theta = imag(hilbert(log(Ic(beta))));
-    
-%end 
-
+%theta = @(x)imag(hilbert(log(Ic(x))));
+f = @(x,y)(x/(2*pi))*(log(Ic(y)) - log(Ic(x)))/(x^2 - y^2);
+for k=1:N
+    theta
+end
 plot(beta,Ic(B))
-plot(beta,theta)
+plot(beta,theta(beta));
     
-transform = Ic(beta).*exp(theta);
+%transform = zeros(size(B));
+Ix = zeros(size(B));
+for k=1:N
+   %transform = @(x)Ic(x).*exp(1j*theta(x)); 
+   exp_part = @(x)exp(1i*(theta(beta(k))-beta(k)*x));
+   ft = @(x)(0.5/pi)*integral(@(beta)Ic(beta(k))*exp_part(x),beta(1),beta(N));
+   Ix(k) = ft(dim(k));
+end
 
-Ix = ifft(transform);
+%Ix = ifft(transform(beta));
 
-plot(beta,Ix,'r',beta,Ic(B),'g');
-plot(beta,Ix);
+%plot(beta,Ix,'r',beta,Ic(B),'g');
+%plot(beta,Ix);
+%plot(beta,log(Ic(beta)));
+%plot(dim,transform(beta));
+%plot(beta,fft(Ix));
+plot(dim,ft(dim));
