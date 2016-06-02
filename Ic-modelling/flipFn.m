6c4a2d3a1ba38d656d60bde5098681d44fa778f9
Shalaka <shalaka-kulkarni@users.noreply.github.com>
2016-06-02 11:33:38 +0100

    Works!

--- Ic-modelling/flipFn.m
+++ Ic-modelling/flipFn.m
@@ -1,16 +1,17 @@
 function [F,minX] = flipFn(Y, X)
-    n = size(X);
+    n_arr = size(X);
+    n = n_arr(2);
     minX  = zeros(size(X));
     minCount = 0;
     buf = 5; %2*buf is the number of consecutive samples examined to determine a minimum
     for i=buf+1:(n-buf)
-        if X(i) == min(X(i-buf:i+buf))
+        if Y(i) == min(Y(i-buf:i+buf))
             minCount = minCount+1;
             minX(minCount) = X(i);
         end
     end
     
-    multiplier = +1;    
+    multiplier = -1;    
     index = 1;
     F = zeros(size(Y));
     for i=1:n
