c21876acdee74c26d804a653d815e5c32d954139
Shalaka <shalaka-kulkarni@users.noreply.github.com>
2016-06-08 11:11:04 +0100

    Works

--- Ic-modelling/getBoundary.m
+++ Ic-modelling/getBoundary.m
@@ -1,6 +1,7 @@
 function [p1,p2] = getBoundary(Y) %note that this fn returns indices p1,p2 and not values
-    slope = diff(Y);
+    slope = gradient(Y);
     n_arr = size(Y); n = n_arr(2);
+    plot(linspace(-1,1,n),slope);
     max = 0; min = 0;
     for i=1:n-1
         if slope(i) > max && slope(i)*slope(i+1)>0
