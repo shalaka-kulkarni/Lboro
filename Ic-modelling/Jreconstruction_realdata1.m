400c9ce284a52c95d765924aabd96a8414113ab8
Shalaka <shalaka-kulkarni@users.noreply.github.com>
2016-06-08 11:09:43 +0100

    Works but with low spatial accuracy

--- Ic-modelling/Jreconstruction_realdata1.m
+++ Ic-modelling/Jreconstruction_realdata1.m
@@ -6,7 +6,7 @@ lambda = 90E-9; %London penetration depth of Niobium
 d = 10E-9; L = 500E-9;
 
 %file i/o
-filename = 'data1.csv';
+filename = 'data3.csv';
 A = csvread(filename);
 
 %physical quantities
@@ -15,7 +15,7 @@ Ic_max = zeros(1,n);
 B = zeros(1,n);
 for i=1:n
     B(i) = A(i,1); %in mT
-    Ic_max(i) = A(i,3); %in mA
+    Ic_max(i) = A(i,2); %in mA
 end
 
 plot(B, Ic_max*1E3)
@@ -25,8 +25,8 @@ ylabel('I_c (uA)')
 
 [I_even, minX] = flipFn(Ic_max,B);
 
-I_odd = interp1(B, I_even, minX);% - Ic_max(floor(n/2));
-%I_odd = zeros(size(I_even)); %debugging
+%I_odd = interp1(B, I_even, minX);% - Ic_max(floor(n/2));
+I_odd = zeros(size(I_even)); %debugging
 %plot(minX,I_odd);
 
 Ix = I_even + 1j*I_odd;
@@ -43,7 +43,7 @@ ylabel('I_c (uA)')
 legend('I_e', 'I_o')
 
 spac_vect = linspace(-L/2,L/2,n);
-a = simFraunhofer(d,L,abs(B(n)),n);
+a = simFraunhofer(d,L,abs(B(n)),n,spac_vect);
 plot(a*spac_vect*1E9, Jx*1E3, '-o');
 title('Current density')
 xlabel('x (nm)')
