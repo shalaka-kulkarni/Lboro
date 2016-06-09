5d99e78a0b06cec3728ece68adc188a5c2ed39d7
Shalaka <shalaka-kulkarni@users.noreply.github.com>
2016-06-09 11:48:45 +0100

    Still running trials

--- Ic-modelling/J_to_Ic.m
+++ Ic-modelling/J_to_Ic.m
@@ -2,12 +2,15 @@
 
 L = 500E-9; w = 100E-9; N = 100;
 
-%J1 = @(x,y)((heaviside(x+L) - heaviside(x-L))*(heaviside(y+w) - heaviside(y-w)));
-%J1 = @(x,y)(sin(x));
-J1 = @(x,y)(x*x/L + 2)*(y*y/w + 2);
+J1 = @(x,y)((heaviside(x+L/2) - heaviside(x-L/2))*(heaviside(y+w/2) - heaviside(y-w/2)));
+% J1 = @(x,y)(sin(x));
+% J1 = @(x,y)gaussmf(x,[1 0]);
+% J1 = @(x,y)(x/L + 2)*(heaviside(x+L/2) - heaviside(x-L/2));
+% J1 = @(x,y)(x/L + 2)*(heaviside(x+L/2) - heaviside(x-L/2))*(y/w + 2)*(heaviside(y+w/2) - heaviside(y-w/2));
+% J1 = @(x,y) (1-abs(x))*(heaviside(x+L/2) - heaviside(x-L/2));
 
-xvec = linspace(-L/2,L/2,N);
-yvec = linspace(-w/2,w/2,N); 
+xvec = linspace(-L,L,N);
+yvec = linspace(-w,w,N); 
 
 %Ix = @(x)integral(@(y)arrayfun(@(x,y)J0(x,y),x,y),-b,b); %,'ArrayValued',true);
 Ix = zeros(size(xvec));
@@ -15,25 +18,19 @@ for i=1:N
     Ix(i) = integral(@(y)J1(xvec(i),y),-w/2,w/2,'ArrayValued',true);
 end
 
-plot(xvec,Ix);
+%plot(xvec,Ix);
 
 kvec = linspace (-1,1,N);
 Ic = @(k)fftshift(abs(fft(Ix)));
 
-plot(kvec,Ic(kvec),'-o');
+Ic_max = Ic(kvec);
 
-%%% try plotting a fraction of the spectrum, doing away with the delta 
+%plot(kvec(N/2+3:end),Ic_max(N/2+3:end),'-o');
+%plot(kvec(3*N/4:end),Ic_max(3*N/4:end),'-o');
 
+plot(kvec, Ic_max, '-o');
+%plot(kvec,spline(kvec,Ic_max,kvec/4),'-o');
 
-%%%%%%%%%%%%%%%%Trials%%%%%%%%%%%%%%%%%
-
-%plot(kvec,fft(sinc(kvec)));
-
-f1 = @(x)fftshift(fft(heaviside(x+1) - heaviside(x-1)));
-%x11 = linspace(0,3,3*N);
-x11 = 0:N;
-%plot(kvec,f1(kvec));
-%plot(linspace(-4,4,500),ifft(heaviside(x+1) - heaviside(x-1)));
 
 
 
