7a2e9933b1b8089041a813c13abd8871e38385ec
Shalaka <shalaka-kulkarni@users.noreply.github.com>
2016-06-10 15:40:18 +0100

    Why is the plot not varying?

--- Ic-modelling/J_to_Ic_2D.m
+++ Ic-modelling/J_to_Ic_2D.m
@@ -1,7 +1,7 @@
 %current density predefined/known
 fluxQuantum = 2.06783383E-15;
-L = 500E-9; w = 100E-9; d = 10E-9; N = 100;
-c = 2*pi*d/fluxQuantum;
+L = 500E-9; w = 500E-9; d = 10E-9; N = 100;
+k = 2*pi*d/fluxQuantum;
 
 pulse = @(t,a)(heaviside(t+a) - heaviside(t-a));
 J1 = @(x,y) pulse(x,L/2)*pulse(y,w/2);
@@ -10,11 +10,14 @@ J1 = @(x,y) pulse(x,L/2)*pulse(y,w/2);
 xvec = linspace(-L,L,N);
 yvec = linspace(-w,w,N); 
 Bwidth = 0.15;
-tth = 1000; %tan theta, theta being the angle betn By and Beff
+theta = 45;
+c = cos(theta);
+s = sin(theta);
 
-By = linspace(-Bwidth,Bwidth,N);
-Bx = tth*By; 
-kx = c*By; ky = c*Bx;
+B = linspace(-Bwidth,Bwidth,N);
+By = c*B;
+Bx = s*B; 
+kx = k*By; ky = k*Bx;
 
 Ix = zeros(size(xvec));
 for i=1:N
@@ -25,7 +28,7 @@ Ic = @(k)fftshift(abs(fft(Ix)));
 Ic_max = Ic(kx);
 
 plot(kvec, Ic_max, '-o');
-% plot(kvec,spline(kvec,Ic_max,kvec/3),'-o');
+plot(kvec,spline(kvec,Ic_max,kvec/3),'-o');
 title('Peak critical current')
 xlabel('B')
 ylabel('I_c')
