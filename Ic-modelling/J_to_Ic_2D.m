a6566cef80b9805d10dd1ad05dcc85c21961f512
Shalaka <shalaka-kulkarni@users.noreply.github.com>
2016-06-13 15:54:16 +0100

    Works!

--- Ic-modelling/J_to_Ic_2D.m
+++ Ic-modelling/J_to_Ic_2D.m
@@ -1,6 +1,6 @@
 %current density predefined/known
 fluxQuantum = 2.06783383E-15;
-L = 500E-9; w = 500E-9; d = 10E-9; N = 100;
+L = 600E-9; w = 600E-9; d = 10E-9; N = 100;
 k = 2*pi*d/fluxQuantum;
 
 pulse = @(t,a)(heaviside(t+a) - heaviside(t-a));
@@ -9,28 +9,28 @@ J1 = @(x,y) pulse(x,L/2)*pulse(y,w/2);
 
 xvec = linspace(-L,L,N);
 yvec = linspace(-w,w,N); 
-Bwidth = 0.15;
+Bwidth = 1;
 theta = 0;
-c = cos(theta);
-s = sin(theta);
+c = cosd(theta);
+s = sind(theta);
 
 B = linspace(-Bwidth,Bwidth,N);
 By = c*B;
 Bx = s*B; 
 kx = k*By; ky = k*Bx;
 
-Ix = zeros(size(xvec)); 
+Ix = zeros(size(xvec)); Ic_max2 = zeros(size(B));
 % Ic_max = zeros(size(B));
 for i=1:N
-    Ix(i) = integral(@(y)(J1(xvec(i),y)*exp(-1j*ky(i)*y)),-w/2,w/2,'ArrayValued',true);
-%     f = @(x)integral(@(y)(J1(xvec(i),y)*exp(-1j*(kx(i)*x-ky(i)*y))),-w/2,w/2,'ArrayValued',true);
-%     Ic_max(i) = abs(integral(@(x)f(x),-L/2,L/2));%,'ArrayValued',true);
+%     Ix(i) = integral(@(y)(J1(xvec(i),y)*exp(-1j*ky(i)*y)),-w/2,w/2,'ArrayValued',true);
+    f = @(x)integral(@(y)(J1(x,y).*exp(-1j*ky(i)*y)),-w/2,w/2,'ArrayValued',true);
+    Ic_max2(i) = abs(integral(@(x)f(x).*exp(1j*kx(i)*x),-L/2,L/2));%,'ArrayValued',true);
 end
-plot(ky,Ix);
-Ic = @(k)fftshift(abs(fft(Ix)));
-Ic_max = Ic(kx);
+% plot(ky,Ix);
+% Ic = @(k)fftshift(abs(fft(Ix)));
+% Ic_max = Ic(kx);
 
-plot(B, Ic_max, '-o');
+plot(B, Ic_max2,'-o');
 % plot(B,spline(B,Ic_max,B/3),'-o');
 title('Peak critical current')
 xlabel('B')
