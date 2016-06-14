9bef47f98f8cc9c011e7d17d09542e4e864ab4c0
Shalaka <shalaka-kulkarni@users.noreply.github.com>
2016-06-14 16:16:20 +0100

    Works; calls J_to_Ic_2D_1 for varying theta

---
+++ Ic-modelling/Ic_2D_var.m
@@ -0,0 +1,25 @@
+
+N = 500; n=5;
+
+Ic = cell(n,N);
+
+for i=0:n-1
+    theta = i*10; %in degrees
+    Ic_max = J_to_Ic_2D_1(theta);
+    Ic{i+1} = Ic_max;
+end
+I0 = Ic{1}(N/2);
+Bwidth = 0.15;
+B = linspace(-Bwidth,Bwidth,N);
+
+leg = cell(1,n);
+figure
+hold all
+for i=1:n
+    plot(B*1E3,Ic{i}*1E6,'LineWidth',1.5);
+    leg{i} = ['\theta = ',num2str((i-1)*10),'^o'];
+end
+title('Peak current variation with rotation of magnetic field')
+xlabel('B (mT)')
+ylabel('I_c (uA)')
+legend(leg)
